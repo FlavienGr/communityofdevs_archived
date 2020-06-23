221329175ec4ccb3b1dd8a75c1dec47b07b6ef14
Flavien <zencles75@gmail.com>
2020-06-23 14:55:20 +0200

    validationdata middleware

---
+++ server/middlewares/validationData.js
@@ -0,0 +1,11 @@
+const { validationResult } = require('express-validator');
+const RequestValidationErrors = require('../errors/request-validation-errors');
+
+const validationData = async (req, _res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new RequestValidationErrors(errors.array()));
+  }
+};
+
+module.exports = validationData;
