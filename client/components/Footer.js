e87525ad38e670951d4cbc69218967ea03c17878
Flavien <zencles75@gmail.com>
2020-07-17 16:15:08 +0200

    footer bootstrap

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,19 +1,32 @@
 import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Footer() {
   return (
     <footer>
-      <div>
-        <Link href="/conditions">
-          <a>{"Conditions générales d'utilisation"}</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/contact">
-          <a>Contact</a>
-        </Link>
-      </div>
-      <div className="copyright">{`${new Date().getFullYear()}, ${`\xA9`} All rights reserved`}</div>
+      <Container
+        fluid
+        className="d-flex flex-column justify-content-between py-2">
+        <Row className="my-5 d-flex">
+          <Col md={12} lg={12} className="mt-1">
+            <Link href="/conditions">
+              <a>{"Conditions générales d'utilisation"}</a>
+            </Link>
+          </Col>
+          <Col md={12} lg={12} className="mt-2">
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </Col>
+        </Row>
+        <Row className="text-center py-1">
+          <Col md={12} lg={12}>
+            {`${new Date().getFullYear()}, ${`\xA9`} All rights reserved`}
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
