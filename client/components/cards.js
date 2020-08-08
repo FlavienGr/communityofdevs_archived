fc002391adba3cc5e257f11ab130ba8645eab003
Flavien <zencles75@gmail.com>
2020-08-08 18:45:57 +0200

    add constant var data in Cards

--- client/components/cards.js
+++ client/components/cards.js
@@ -1,29 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Card from './Card';
-
-const data = [
-  {
-    desc: 'Définissez vos besoins',
-    number: 1,
-    img: '/assets/images/idea-icon.svg',
-    alt: 'image idée du project'
-  },
-  {
-    desc: 'Proposez votre projet en ligne',
-    number: 2,
-    img: '/assets/images/send-icon.svg',
-    alt: 'image soumettre son project'
-  },
-  {
-    desc: 'Communiquez directement avec des développeurs',
-    number: 3,
-    img: '/assets/images/chat-icon.svg',
-    alt: 'image chat avec des développeurs'
-  }
-];
+import CustomCards from '../constants/customCards';
 
 export default function Cards() {
-  const returnDescription = data.map(item => (
+  const returnDescription = CustomCards.map(item => (
     <Card
       desc={item.desc}
       key={item.number}
---
+++ client/constants/customCards.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    desc: 'Définissez vos besoins',
+    number: 1,
+    img: '/assets/images/idea-icon.svg',
+    alt: 'image idée du project'
+  },
+  {
+    desc: 'Proposez votre projet en ligne',
+    number: 2,
+    img: '/assets/images/send-icon.svg',
+    alt: 'image soumettre son project'
+  },
+  {
+    desc: 'Communiquez directement avec des développeurs',
+    number: 3,
+    img: '/assets/images/chat-icon.svg',
+    alt: 'image chat avec des développeurs'
+  }
+];
